6b6e37f87ae197b3c33a65a8d361c8581b1de9a5
御坂17032号 <40137262+Misaka17032@users.noreply.github.com>
2020-04-11 11:52:38 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import json
 from pypinyin import lazy_pinyin
 def yayun(s1, s2):
 	y = ["a", "o", "e", "i", "u"]
-	e = ["ai", "ei", "ao", "ou", "an", "en", "ia", "ie", "in", "ue", "un", "ui"]
+	e = ["ai", "ei", "ao", "ou", "an", "en", "ia", "ie", "in", "ue", "un", "ui", "iu"]
 	s = ["ong", "ang", "eng"]
 	if s1[-3:] == s2[-3:] and s1[-3:] in s:
 		return True
@@ -12,12 +12,13 @@ def yayun(s1, s2):
 		return True
 	return False
 poet = []
+f = []
 last_pinyin = ""
 matched = False
 fos = int(input("生成五言诗或七言诗:（5/7）"))
 for i in range(0, 58):
-	f = open("./poet/poet.tang." + str(i * 1000) + ".json","r")
-	s = f.read()
+	fin = open("./poet/poet.tang." + str(i * 1000) + ".json","r")
+	s = fin.read()
 	temp = json.loads(s)
 	for j in temp:
 		for m in j["paragraphs"]:
@@ -27,18 +28,29 @@ for i in range(0, 58):
 			elif len(m) == 16 and fos == 7:
 				poet.append(m[0:7])
 				poet.append(m[8:15])
+fin.close()
 target = input("输入要生成的语句：")
 for i in poet:
 	if i[0] == target[0]:
-		last_pinyin = lazy_pinyin(i[len(i) - 1])[0]
-		print("生成结果：\n" + i)
+		f.append(i)
 		matched = True
-		break
 if matched:
-	for i in range(1, len(target)):
-		for j in poet:
-			if j[0] == target[i][0] and yayun(last_pinyin, lazy_pinyin(j[len(j) - 1])[0]):
-				print(j)
-				break
+	matched = False
+	for m in f:
+		ans = [m]
+		last_pinyin = lazy_pinyin(m[len(m) - 1])[0]
+		for i in range(1, len(target)):
+			for j in poet:
+				if j[0] == target[i] and yayun(last_pinyin, lazy_pinyin(j[len(j) - 1])[0]):
+					ans.append(j)
+					break
+		if len(ans) == len(target):
+			matched = True
+			print("生成结果：")
+			for i in ans:
+				print(i)
+			break
+	if not matched:
+		print("没有找到符合" + target + "的诗句组合")
 else:
 	print("没有找到以" + target[0] + "字开头的诗句")
